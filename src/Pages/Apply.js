79f90b22141bc55c16a56e5916836929c91ad6ed
Justin <jiangj2620@gmail.com>
2021-04-30 14:50:07 -0500

    merge 1

--- src/Pages/Apply.js
+++ src/Pages/Apply.js
@@ -22,8 +22,7 @@ function Apply(props){
                         <h className="apply-largetext">Create joy at Cornell</h>
 
                         <div className="apply-textblock">
-                            <h className="apply-smalltext">Find your fit and help us build real apps</h>
-                            <h className="apply-smalltext">that impact students everyday.</h>
+                            <h className="apply-smalltext">Find your fit and help us build real apps that impact students everyday.</h>
                         </div>
                        
                     </div>
@@ -37,12 +36,9 @@ function Apply(props){
                 <div className="apply-home">
 
                     <div className="apply-center2">
-                        <h className="apply-largetext">Build beautiful, funcitonal apps</h>
-
+                        <h className="apply-largetext">Build beautiful, functional apps</h>
                         <div className="apply-textblock">
-                            <h className="apply-smalltext">Founded in 2014 by a group of students who rallied</h>
-                            <h className="apply-smalltext">around building apps, we've grown into a talented</h>
-                            <h className="apply-smalltext">group of people who love bringing ideas to life.</h>
+                            <h className="apply-smalltext">Founded in 2014 by a group of students who rallied around building apps, we've grown into a talented group of people who love bringing ideas to life.</h>
                         </div>
                         
                     </div>
@@ -54,29 +50,27 @@ function Apply(props){
 
             <div className="container-3">
 
-                <div className="apply-recruitment-top">
+                <div className="apply-recruitment-top container-3-padded">
                     <p className="application-status">Applications are closed</p> 
 
                     <div className="recruit-textblock">
                         <h className="apply-largetext">Fall 2020 Recruitment</h>
 
-                        <h className="apply-smalltext">Dont' worry, no jump scares here. We'll update this</h>
-                        <h className="apply-smalltext">timeline as we figure out the details.</h>
+                        <h className="apply-smalltext">Dont' worry, no jump scares here. We'll update this timeline as we figure out the details.</h>
                     </div>
 
 
                 </div>
 
-                <div className="apply-recruitment-timeline" style={{marginTop:'5%'}}>
-                    <img src={timeline}/>
+                <div className="apply-recruitment-timeline" style={{marginTop:'5%', paddingRight:'5%'}}>
+                    <img src={timeline} style={{width:'100%'}}/>
                 </div>
 
-                <div className="apply-recruitment-mid">
+                <div className="apply-recruitment-mid container-3-padded">
+
                   <h className="apply-medtext">Hear more about our work and chat with members</h>
-                </div>
 
-
-                <div className="apply-cards">
+                  <div className="apply-cards container-3-padded">
                     <LearnMoreCard
                     date = "Monday, September 16"
                     title = "Cornell AppDev Info Session"
@@ -89,6 +83,11 @@ function Apply(props){
                     description = "Cornell AppDev is recruiting developers, designers, and officers to join our Core Team! Come out and talk to our core team members to learn more."
                     />               
                 </div>
+
+                </div>
+
+
+               
                 
 
             </div>
